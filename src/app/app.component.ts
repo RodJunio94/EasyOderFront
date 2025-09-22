716b1f326c86cd86c48f7d1f4bed98f39b503c5f
Junio Rodrigues <junio@buildbox.com.br>
2025-09-21 22:44:41 -0300

    Adiciona configuração de HTTP e exibição de produtos na aplicação Angular. Atualiza angular.json com ID de análise e remove conteúdo de placeholder do app.component.html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'EasyOderFront';
+  http = inject(HttpClient);
+  urlApi = 'https://localhost:7110';
+  products$?: Observable<Product[]>;
+  
+  ngOnInit(): void {
+    this.ProductList();
+  }
+
+  ProductList() {
+    this.products$ = this.http.get<Product[]>(`${this.urlApi}/api/product`)
+  }
 }
